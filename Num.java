28ba605c4d0a0da138f0f3b5e4137117d6a1c5c3
AAP127 <apestonj@gmail.com>
2022-02-04 12:42:48 -0800

    Added files and libs

---
+++ Num.java
@@ -0,0 +1,11 @@
+class Num{
+
+    public static int number(){
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.print(number());
+    }
+
+}
---
+++ NumTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class NumTest {
+    
+    @Test
+    public void testNum(){
+        assertEquals(2, Num.number());
+    }
+    
+}
